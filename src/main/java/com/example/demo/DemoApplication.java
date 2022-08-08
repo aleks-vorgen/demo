977fb3e7f9bebbd3ee60dcc6a0e5fcd25b5b4488
ALexus <vorgen.morgen@yandex.ru>
2022-08-08 11:36:20 +0200

    UTF-8 encoding

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 @SpringBootApplication(scanBasePackages = "com.example.demo")
 public class DemoApplication extends SpringBootServletInitializer {
@@ -11,6 +16,15 @@ public class DemoApplication extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		return builder.sources(DemoApplication.class);
+	}
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		super.onStartup(servletContext);
+		FilterRegistration charEncodingFilterReg = servletContext.addFilter("CharacterEncodingFilter", CharacterEncodingFilter.class);
+		charEncodingFilterReg.setInitParameter("encoding", "UTF-8");
+		charEncodingFilterReg.setInitParameter("forceEncoding", "true");
+		charEncodingFilterReg.addMappingForUrlPatterns(null, false, "/*");
 	}
 
 	public static void main(String[] args) {
